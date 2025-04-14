01af3ba4bdbca6e62e4a99599014715bf339faaf
Ronny Duchateau <ronny.duchateau.da@gmail.com>
2025-04-14 09:21:33 +0000

    EXO2 DBT ADVANCED

--- models/intermediate/int_operational_margin.sql
+++ models/intermediate/int_operational_margin.sql
@@ -1,14 +1,13 @@
-SELECT 
-orders_id
-,quantity
-,date_date
-,revenue
-,purchase_cost
-,margin
-,shipping_fee
-,logcost
-,ship_cost
-,round(margin+shipping_fee-logcost-ship_cost,2) AS operational_margin
-FROM {{ ref('int_orders_margin') }}
-JOIN {{ ref('stg_raw__ship') }}
-USING (orders_id)
+select
+    orders_id,
+    quantity,
+    date_date,
+    revenue,
+    purchase_cost,
+    margin,
+    shipping_fee,
+    logcost,
+    ship_cost,
+    round(margin + shipping_fee - logcost - ship_cost, 2) as operational_margin
+from {{ ref("int_orders_margin") }}
+join {{ ref("stg_raw__ship") }} using (orders_id)
